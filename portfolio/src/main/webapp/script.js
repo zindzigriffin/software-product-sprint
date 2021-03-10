552b7115cd7c7e76a2de81b467c37348cd95bc57
Zindzi Griffin <zindzigriffin@gmail.com>
2021-03-10 22:04:30 +0000

    Added google maps to my portoflio page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -37,3 +37,9 @@ async function showServerTime() {
 
 
 }
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
